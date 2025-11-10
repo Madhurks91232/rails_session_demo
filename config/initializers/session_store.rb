5547ba090460d730c535f8034379752fcba71f26
Madhurks91232 <madhu.rks1991@gmail.com>
2025-11-09 21:14:33 -0500

    Adds Client session store configuration

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,6 @@
+Rails.application.config.session_store :cookie_store,
+  key: '_rails_session_demo_session',
+  httponly: true,
+  secure: Rails.env.production?,   # true in prod; false on localhost
+  same_site: :lax,
+  expire_after: 14.days
